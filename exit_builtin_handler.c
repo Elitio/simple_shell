d45f1b33b69a0380ccb9622769349bd4b44bd4d0
Richard Boakye Duah <richrichie784@gmail.com>
2023-07-23 15:21:11 -0700

    Adds files

---
+++ exit_builtin_handler.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+
+
+/**
+ * exit_shell - handles the behavior of the "exit" command.
+ * @shell_data: structure parameter
+ * Return: 0 on success otherwise 1 to indicate an error.
+ */
+int exit_shell(data_shell *shell_data)
+{
+	unsigned int stat_val = 0;
+	int count;
+	char *arg_str = shell_data->args[1];
+
+	if (arg_str != NULL)
+	{
+
+		for (count = 0; arg_str[count] != '\0'; count++)
+		{
+			if (arg_str[count] < '0' || arg_str[count] > '9')
+			{
+				get_error(shell_data, 2);
+				shell_data->status = 2;
+				return (1);
+			}
+			stat_val = stat_val * 10 + (arg_str[count] - '0');
+		}
+
+		if (stat_val > INT_MAX)
+		{
+			get_error(shell_data, 2);
+			shell_data->status = 2;
+			return (1);
+		}
+
+		shell_data->status = stat_val % 256;
+	}
+
+	return (0);
+}
+
