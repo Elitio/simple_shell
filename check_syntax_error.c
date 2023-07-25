293dd23049dbe27a5061b6316ddd78ce757c01f8
Richard Duah Boakye <richrichie784@gmail.com>
2023-07-25 14:00:39 +0000

    Update check_syntax_error.c

--- check_syntax_error.c
+++ check_syntax_error.c
@@ -1,117 +1,102 @@
 #include "main.h"
 
-/**
- * repeated_char - counts the repetition of a char
- *
- * @input: input string
- * @i: index
- * Return: repetitions
- */
-
-int repeated_char(char *input, int i)
-{
-	if (*(input - 1) == *input)
-	{
-		return (repeated_char(input - 1, i + 1));
-	}
-
-	return (i);
-}
 
 /**
- * error_sep_op - Will finds syntax errors
- * @input: input string
- * @i: index
- * @last: last char read
- * Return: index of error. 0 when there are no errors
+ * check_syntax_error - intermediate function to
+ * find and print a syntax error
+ * @shell_data: structure
+ * @inp_str: input string
+ * Return: 1 if there is an error. 0 in other case
  */
-int error_sep_op(char *input, int i, char last)
+int check_syntax_error(data_shell *shell_data, char *inp_str)
 {
-	int count;
+	int start_ind = 0, err_ind;
+	int reslt = first_char(inp_str, &start_ind);
 
-	count = 0;
-	if (*input == '\0')
-	{
-		return (0);
-	}
-
-	if (*input == ' ' || *input == '\t')
-	{
-		return (error_sep_op(input + 1, i + 1, last));
-	}
-
-	if (*input == ';')
-		if (last == '|' || last == '&' || last == ';')
+	do {
+		if (reslt == -1)
 		{
-			return (i);
+			print_syntax_error(shell_data, inp_str, start_ind, 0);
+			return (1);
 		}
 
-	if (*input == '|')
-	{
-		if (last == ';' || last == '&')
+		err_ind = error_sep_op(inp_str + start_ind, 0,
+				inp_str[start_ind]);
+		if (err_ind != 0)
 		{
-			return (i);
+			print_syntax_error(shell_data, inp_str, start_ind
+					+ err_ind, 1);
+			return (1);
 		}
 
-		if (last == '|')
-		{
-			count = repeated_char(input, 0);
-			if (count == 0 || count > 1)
-			{
-				return (i);
-			}
-		}
-	}
-
-	if (*input == '&')
-	{
-		if (last == ';' || last == '|')
-		{
-			return (i);
-		}
-
-		if (last == '&')
-		{
-			count = repeated_char(input, 0);
-			if (count == 0 || count > 1)
-			{
-				return (i);
-			}
-		}
-	}
-
-	return (error_sep_op(input + 1, i + 1, *input));
-}
-
-
-/**
- * check_syntax_error - Finds and print a syntax error.
- *
- * @datash: a data structure.
- * @input: Input string.
- * 
- * Return: Will return 1 for error, and 0 for otherwise.
- */
-
-int check_syntax_error( data_shell *datash, char *input)
-{
-	int begin = 0;
-	int fir_st_char = 0;
-	int i = 0;
-
-	fir_st_char = first_char(input, &begin);
-	if (fir_st_char == -1)
-	{
-		print_syntax_error(datash, input, begin, 0);
-		return (1);
-	}
-
-	i = error_sep_op(input + begin, 0, *(input + begin));
-	if (i != 0)
-	{
-		print_syntax_error(datash, input, begin + i, 1);
-		return (1);
-	}
+	} while (0);
 
 	return (0);
 }
+
+
+
+/**
+ * repeated_char - counts the number of repeated occurrences of a character
+ * in a string, starting from a given position.
+ * @inp_str: input string
+ * @ind: current count of repeated occurrences.
+ * Return: current count ind if characters are not equal
+ */
+int repeated_char(char *inp_str, int ind)
+{
+	int count = 0;
+
+	while (*(inp_str + ind) == *(inp_str + ind - 1))
+	{
+		count++;
+
+		ind++;
+	}
+
+	return (count);
+}
+
+
+
+
+
+/**
+ * error_sep_op - finds syntax errors
+ *
+ * @inp_str: input command line string.
+ * @ind: index of the current character in the input string
+ * @ind_sep_end: last separator character encountered before the current index.
+ * Return: 0 on no syntax errors related to separators.
+ */
+int error_sep_op(char *inp_str, int ind, char ind_sep_end)
+{
+	int count;
+
+	if (*inp_str == '\0')
+		return (0);
+
+	if (*inp_str == ' ' || *inp_str == '\t')
+		return (error_sep_op(inp_str + 1, ind + 1, ind_sep_end));
+
+	switch (*inp_str)
+	{
+		case ';':
+			if (ind_sep_end == '|' || ind_sep_end == '&'
+					|| ind_sep_end == ';')
+				return (ind);
+			break;
+		case '|':
+		case '&':
+			if (ind_sep_end == ';' || ind_sep_end == '|')
+			{
+				count = repeated_char(inp_str, 0);
+				if (count == 0 || count > 1)
+					return (ind);
+			}
+			break;
+	}
+
+	return (error_sep_op(inp_str + 1, ind + 1, *inp_str));
+}
+
