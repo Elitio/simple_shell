dc32e666750d848531d5941b30b0268a1c90d60f
Richard Duah Boakye <richrichie784@gmail.com>
2023-07-25 13:53:28 +0000

    Update print_syntax_error.c

--- print_syntax_error.c
+++ print_syntax_error.c
@@ -1,53 +1,54 @@
 #include "main.h"
 
 /**
- * print_syntax_error - prints when a syntax error is found
- * @datash: data structure
- * @input: input string
- * @i: index of the error
- * @bool: to control message errors
- * Return: no return
+ * print_syntax_error - prints errors when parsing the input commands
+ * @shell_data: structure parameter
+ * @inp_str: input command string parameter
+ * @ind: current char index parameter
+ * @isSingle: flag to determine whether it is the first or second semicolon
+ * Return: nothing
  */
-void print_syntax_error(data_shell *datash, char *input, int i, int bool)
+void print_syntax_error(data_shell *shell_data, char *inp_str, int ind,
+		int isSingle)
 {
-	char *msg, *msg2, *msg3, *error, *counter;
-	int length;
+	char *char_holder = NULL, *err1_msg = ": Oops! error: \"";
+	char *err2_msg = "\" caused error\n", *err_full;
+	char *cmdNum = aux_itoa(shell_data->counter);
+	int err_len;
 
-	if (input[i] == ';')
+	switch (inp_str[ind])
 	{
-		if (bool == 0)
-			msg = (input[i + 1] == ';' ? ";;" : ";");
-		else
-			msg = (input[i - 1] == ';' ? ";;" : ";");
+		case ';':
+			char_holder = (isSingle == 0 ? (inp_str[ind + 1]
+						== ';' ? ";;" : ";")
+					: (inp_str[ind - 1] == ';'
+						? ";;" : ";"));
+			break;
+		case '|':
+			char_holder = (inp_str[ind + 1] == '|' ? "||" : "|");
+			break;
+		case '&':
+			char_holder = (inp_str[ind + 1] == '&' ? "&&" : "&");
+			break;
 	}
-
-	if (input[i] == '|')
-		msg = (input[i + 1] == '|' ? "||" : "|");
-
-	if (input[i] == '&')
-		msg = (input[i + 1] == '&' ? "&&" : "&");
-
-	msg2 = ": Syntax error: \"";
-	msg3 = "\" unexpected\n";
-	counter = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(counter);
-	length += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
-
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
+	if (char_holder != NULL)
 	{
-		free(counter);
-		return;
-	}
-	strcpy(error, datash->av[0]);
-	strcat(error, ": ");
-	strcat(error, counter);
-	strcat(error, msg2);
-	strcat(error, msg);
-	strcat(error, msg3);
-	strcat(error, "\0");
+		err_len = _strlen(shell_data->av[0]) + _strlen(cmdNum)
+			+ _strlen(char_holder) + _strlen(err1_msg)
+			+ _strlen(err2_msg) + 2;
+		err_full = malloc(sizeof(char) * (err_len + 1));
 
-	write(STDERR_FILENO, error, length);
-	free(error);
-	free(counter);
+		if (err_full != NULL)
+		{
+			_strcpy(err_full, shell_data->av[0]);
+			_strcat(err_full, ": ");
+			_strcat(err_full, cmdNum);
+			_strcat(err_full, err1_msg);
+			_strcat(err_full, char_holder);
+			_strcat(err_full, err2_msg);
+			write(STDERR_FILENO, err_full, err_len);
+			free(err_full);
+		}
+	}
+	free(cmdNum);
 }
