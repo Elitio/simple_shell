e9aef1c9d3cef65ff5a91f9e500e039045f3d036
Richard Duah Boakye <richrichie784@gmail.com>
2023-07-27 04:32:31 +0000

    Update prompt_loop.c

--- prompt_loop.c
+++ prompt_loop.c
@@ -8,44 +8,42 @@
 
 void shell_loop(data_shell *data_shell_info)
 {
-    int count = 1;
-    char *user_input;
-    int the_end_of_file;
+	int count = 1;
+	char *user_input;
+	int the_end_of_file;
 
-    do {
-        write(STDOUT_FILENO, "$ ", 2);
-
-        user_input = read_line(&the_end_of_file);
-        if (the_end_of_file != -1)
-        {
-            user_input = without_comment(user_input);
-            if (user_input == NULL)
-            {
-                continue;
-            }
-            switch (check_syntax_error(data_shell_info, user_input))
-            {
-                case 1:
-                    data_shell_info->status = 2;
-                    break;
-                default:
-                    break;
-            }
-            if (data_shell_info->status == 2)
-            {
-                free(user_input);
-                continue;
-            }
-            user_input = rep_var(user_input, data_shell_info);
-            count = split_commands(data_shell_info, user_input);
-            data_shell_info->counter += 1;
-        }
-        else
-        {
-            count = 0;
-        }
-        free(user_input);
-    } while (count == 1);
+	do {
+		write(STDOUT_FILENO, "$ ", 2);
+		user_input = read_line(&the_end_of_file);
+		if (the_end_of_file != -1)
+		{
+			user_input = without_comment(user_input);
+			if (user_input == NULL)
+			{
+				continue;
+			}
+			switch (check_syntax_error(data_shell_info, user_input))
+			{
+				case 1:
+					data_shell_info->status = 2;
+					break;
+				default:
+					break;
+			}
+			if (data_shell_info->status == 2)
+			{
+				free(user_input);
+				continue;
+			}
+			user_input = rep_var(user_input, data_shell_info);
+			count = split_commands(data_shell_info, user_input);
+			data_shell_info->counter += 1;
+		}
+		else
+		{
+			count = 0;
+		}
+		free(user_input);
+	} while (count == 1);
 }
-
 
