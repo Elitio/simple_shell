7478f50485d9c0ff1b95e070d6d2352ce5102917
Christian Annan Segbenu <elitio@gmail.com>
2023-07-29 14:55:04 -0700

    Correcting errors

--- call_getline.c
+++ call_getline.c
@@ -8,10 +8,15 @@
 */
 char *callGetLineF(int *is_end_of_file)
 {
+	/* Declare variables to store the input string and its size */
 	char *input_str = NULL;
 	size_t size = 0;
 
+	/* Call the function getLineF to read the input from
+	 * the user via stdin (standard input)
+	 * */
 	*is_end_of_file = getLineF(&input_str, &size, stdin);
 
+	/* Return the input string read from the user */
 	return (input_str);
 }
